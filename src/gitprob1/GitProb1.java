6549ed86d272eaa0df76a4443c79705c3837d9e9
Marcus Henriksson <marcus.henriksson@doceoconsulting.se>
2021-02-14 19:15:02 +0100

    Lagt till rad om ny rad

--- src/gitprob1/GitProb1.java
+++ src/gitprob1/GitProb1.java
@@ -17,6 +17,7 @@ public class GitProb1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Detta är en ny rad!");
     }
     
 }
